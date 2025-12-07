166ae1f45e786d90d20c0325cf2b424bdbbe0f5e
Tiramisu47 <Tiramisu47@users.noreply.github.com>
2025-12-07 21:53:17 +0100

    Cutscene
    Working cutscene of the flashback, also some minor changes.

---
+++ Ashes-Of-The-Eve/Assets/Timelines/SceneChangerOnTimer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerOnTimer : MonoBehaviour
+{
+    public float changeTime;
+
+    private void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if (changeTime <= 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
